c2f2ba93bfabed251c6a5cd2a36a244830ef9b68
redWallaroo <sandra.f.lombardi@gmail.com>
2012-11-07 12:46:33 -0500

    Modified Recursive search, updated Readme

--- README.md
+++ README.md
@@ -6,9 +6,10 @@ Description: A grep-like command utility providing various seach options.
 	Usage: Seek.py [-o | --options] [search_term] [search_location]
 
 ### EXAMPLE: 
+		seek.py search_term (searches all files within current directory)
 		seek.py seach_term /path/to/file (searches that file only)
 		seek.py "search_term" /path/to/file (same as above)
-		seek.py search_term *.extension  (searches within current directory)
+		seek.py search_term *.extension  (searches files of .extension within current directory)
 
 ### NOTES: 
 
@@ -24,17 +25,19 @@ OPTIONS
 Seek.py supports the following search options:
 
 ### -p | --pattern_file :
-Pull search_terms from a file. Search terms must be separated by a newline:
+Pull search_terms from a given file. Search terms must be separated by a newline:
 
-		seek.py -p path/to/search/file path/to/file
+		seek.py -p [path/to/search_term/file] [path/to/search_location]
 		seek.py -p words.txt text_file.txt
-		seek,py -p words.txt *.extension
+		seek.py -p words.txt *.extension
+		seek.py -p words.txt (searches all files within current directory)
 
 ### -i | --ignore_case:   
 Performs case-insensitve search.
 		
 		seek.py -i search_term path/to/file
 		seek.py -i search_term *.extension
+		seek.py -i search_term (searches all files within current directory)
 					  
 (returns all lines matching search_term, SEARCH_TERM, SearcH_TERm etc. from path/to/file)					  		   
 
@@ -43,6 +46,7 @@ Searches for any lines with words that start with search term.
 
 		seek.py -S ten /path/to/file
 		seek.py -S ten *.extension
+		seek.py -S ten (searches all files within current directory)
 
 (returns all lines including words such as tennis, tenure, tent, etc)
 
@@ -51,12 +55,14 @@ Searches for any lines where the line starts with search term.
 
 		seek.py -s search_term /path/to/file
 		seek.py -s search_term *.extension
+		seek.py -s search_term (searches all files within current directory)
 
 ### -E | --ends_with:
 Searches for any lines with words that end with search term.
 
 		seek.py -S ted /path/to/file
 		seek.py -S ted *.extension
+		seek.py -S ted (searches all files within current directory)
 
 (returns all lines including words such as painted, invited, etc)
 
@@ -65,17 +71,20 @@ Searches for any lines where the line ends with search term.
 
 		seek.py -s search_term /path/to/file
 		seek.py -s search_term *.extension
+		seek.py -s search_term (searches all files within current directory)
 
 ### -w | --match_whole_word:
 Searches for any lines with words that as a whole match the search_term.
 
 		seek.py -w search_term /path/to/file
 		seek.py -w search_term *.extension
+		seek.py -w search_term (searches all files within current directory)
 
 ### -r | --recursive_dir:
 Searches recursively for any lines containing search_term in files within indicated directory.
 
 		seek.py -r search_term /path/to/directory
+		seek.py -r search_term (searches current directory recursively)
 		seek.py -r search_term *.extension (searches current directory recursively for files with .extension)
 
 (returns filename: line number: line text)
@@ -83,14 +92,15 @@ Searches recursively for any lines containing search_term in files within indica
 ### -l | --list_filenames:
 Searches for any files containing search_term in current directory and returns filenames
 
-		seek.py -l search_term
-		seek.py -l search_term *.extension
+		seek.py -l search_term (searches current directory)
+		seek.py -l search_term *.extension (searches current directory for files with .extension)
 
 ### -y | --synonym_search:
 Searches for any files containing a match for the search_term and *any* of the synonym words returned from the dictionary.
 
 		seek.py -y search_term /parth/to/file
 		seek.py -y search_term *.extension
+		seek.py -y search_term (searches files in current directory)
 
 
 
--- seek_functions.py
+++ seek_functions.py
@@ -9,65 +9,64 @@ from sets import Set
 
 
 # search
-def search(args, ignore_case_flag = 0):
+def search(args, ignore_case = 0):
     
     Found_in_lines = {}
     search_pattern = args[0]
     text_lines = read_lines_from_file(args[1])
     for line in range(len(text_lines)):
-        search_result = re.search(search_pattern,text_lines[line],ignore_case_flag)
+        search_result = re.search(search_pattern,text_lines[line],ignore_case)
         if search_result:
-            result_line = re.sub(search_pattern, highlight, text_lines[line], flags = ignore_case_flag)
+            result_line = re.sub(search_pattern, highlight, text_lines[line], flags = ignore_case)
             key, value = line, result_line
             Found_in_lines[key] = value
     return Found_in_lines, search_pattern
 
-def search_helper(arguments,search_location, ignore_case_flag = 0, print_filename_flag = 0, print_line_flag = 0):
-    if len(search_location) > 1: print_filename_flag = 1
+def search_helper(arguments, search_location, ignore_case = 0, print_filename = 0, print_line = 0):
+
+    if search_location == os.getcwd().split():
+        search_location = files_in_current_directory(search_location)
+    
+    if len(search_location) > 1: print_filename = 1
     for x in range (len(search_location)):
         args = arguments, search_location[x]
-        results, search_terms = search(args, ignore_case_flag)
+        results, search_terms = search(args, ignore_case)
         if search_terms.find("|"):
             search_terms = search_terms.split('|')
         else:
             search_terms = search_terms.split()   
         unique_terms = list(set(search_terms))
         filename = search_location[x]
-        print_results(results, unique_terms, filename, print_filename_flag, print_line_flag)
+        print_results(results, unique_terms, filename, print_filename, print_line)
 
 
 # search options
 
-def basic_search(args, ignore_case_flag = 0):
-    search_helper(args[0],args[1],ignore_case_flag)
+def basic_search(args, ignore_case = 0):
+    search_helper(args[0], args[1], ignore_case, print_line = 1)
 
 def line_starts_with(args):
     search_arg = '^'+ args[0]
-    search_helper(search_arg, args[1])
+    search_helper(search_arg, args[1], print_line = 1)
 
 def starts_with(args):
     search_arg = '\\b'+ args[0] + '\\B'
-    search_helper(search_arg, args[1])
+    search_helper(search_arg, args[1], print_line = 1)
 
 def ends_with(args):
     search_arg = '\\B' + args[0] + '\\b'
-    search_helper(search_arg, args[1])
+    search_helper(search_arg, args[1], print_line = 1)
     
 def line_ends_with(args):
     search_arg = args[0] + '$'
-    search_helper(search_arg, args[1])
+    search_helper(search_arg, args[1], print_line = 1)
 
 def match_whole_word(args):
     search_arg = '\\b' + args[0] + '\\b'
-    search_helper(search_arg, args[1])
+    search_helper(search_arg, args[1], print_line = 1)
     
 def list_filenames(args):
-    if len(args[1]) == 1:
-        files = files_in_current_directory(args[1])
-        print ("Searching " + str(len(files)) + " files...")
-        search_helper(args[0],files, print_filename_flag = 1)
-    elif len(args[1]) > 1:
-        search_helper(args[0],args[1], print_filename_flag = 1)
+    search_helper(args[0],args[1], print_filename = 1)
 
 def pattern_file(args):
     search_terms = []
@@ -75,23 +74,26 @@ def pattern_file(args):
     for line in text_lines:
         search_terms.append(line.strip('\n'))
     arguments = '|'.join(search_terms)
-    search_helper(arguments,args[1], print_line_flag = 1)
+    search_helper(arguments,args[1], print_line = 1)
 
 
 def recursive_dir(args):
+    
     if len(args[1]) == 1:
         files = files_in_recursive_directory(args[1])
-        print ("Searching " + str(len(files)) + " files...")
-        search_helper(args[0],files, print_line_flag = 1)
+        search_helper(args[0],files, print_line = 1)
     elif len(args[1]) > 1:
-        search_helper(args[0],args[1], print_line_flag = 1)
+        # Assumes filename wildcard to use in recursive search
+        file_extension = find_file_extension(args[1])
+        files = files_in_recursive_directory(os.getcwd().split(), extension = file_extension)
+        search_helper(args[0],files, print_line = 1)
       
 
 def synonym_search(args):
     search_word = args[0]
     synonyms = find_synonyms(search_word)
     print "Searching for the following synonym words: " + synonyms.replace('|',',')
-    search_helper(synonyms,args[1], print_line_flag = 1)
+    search_helper(search_word, args[1], print_line = 1)
 
 
 def find_synonyms(search_word):
@@ -105,6 +107,10 @@ def find_synonyms(search_word):
 
 # Common Functions
 
+def find_file_extension(arg):
+    filename, extension = os.path.splitext(arg[0])
+    return extension
+
 def files_in_recursive_directory(directory_name, extension = None):
     list_of_files = []
     for dirs, subdirs, files in os.walk(directory_name[0]):
@@ -112,7 +118,8 @@ def files_in_recursive_directory(directory_name, extension = None):
             if extension == None:
                 list_of_files.append(os.path.join(dirs,i))
             else:
-                pass
+                if i.endswith(extension) == True: 
+                    list_of_files.append(os.path.join(dirs,i))
     return list_of_files   
     
 def files_in_current_directory(directory_name, extension = None):
@@ -130,12 +137,12 @@ def highlight(matchobj):
     return '\033[92m' + matchobj.group(0) + '\033[0m'
 
 
-def print_results(results, search_terms, filename = None, print_filename_flag = 0, print_line_flag = 0):
+def print_results(results, search_terms, filename = None, print_filename = 0, print_line = 0):
     
     for key,value in results.iteritems():
-            if print_filename_flag == 1 and print_line_flag == 1:
+            if print_filename == 1 and print_line == 1:
                 print '\n' + '\033[94m' + filename + '\033[0m' + '\033[91m' + " (Line " + str(key) + ")" + '\033[0m' + str(value).strip(' ')
-            elif print_filename_flag == 1 and print_line_flag == 0:
+            elif print_filename == 1 and print_line == 0:
                 print '\n' + '\033[94m' + filename + '\033[0m' + '\033[91m' + " (Line " + str(key) + ")" + '\033[0m'
             else:
                 print '\n' + '\033[91m' + "(Line " + str(key) + ")" + '\033[0m' + str(value).strip(' ')
